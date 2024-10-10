e4f5d954bc03342f95a80c32e6ba686ca9492c29
vermashaurya24 <vermashaurya24@gmail.com>
2024-10-10 16:01:52 +0545

    feat: Rendered the logo on the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 const Header = () => {
-    return (<div>
-        <h1>Header</h1>
-    </div>);
-}
+  return (
+    <div className="header-container">
+      <div className="logo-container">
+        <img src="https://media.licdn.com/dms/image/v2/C4D0BAQFeP0T2C9Io9g/company-logo_200_200/company-logo_200_200/0/1630527219302?e=1736380800&v=beta&t=4DuGKbRoOGZsksvvSIsUwllcciPGDAhRrboC-Ep9OHg" />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
