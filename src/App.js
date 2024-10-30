5cb4c9e605bce1af26a903b1d3b6c2e6bfc41086
vermashaurya24 <vermashaurya24@gmail.com>
2024-10-31 01:03:27 +0545

    chore: Minor design changes.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Header from "./components/Header";
 
 const App = () => {
   return (
-    <>
+    <div className="mt-3 mx-3">
       <Header />
       <h1>Body Here</h1>
-    </>
+    </div>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div className="flex justify-between items-center mt-3 mx-3 rounded-xl bg-gradient-to-l from-blue-400">
+    <div className="flex justify-between items-center border-solid border-2 border-sky-500 rounded-xl bg-gradient-to-l from-blue-300">
       <div>
-        <img className="w-24" src="https://media.licdn.com/dms/image/v2/C4D0BAQFeP0T2C9Io9g/company-logo_200_200/company-logo_200_200/0/1630527219302?e=1736380800&v=beta&t=4DuGKbRoOGZsksvvSIsUwllcciPGDAhRrboC-Ep9OHg" />
+        <img className="w-24 rounded-2xl" src="https://media.licdn.com/dms/image/v2/C4D0BAQFeP0T2C9Io9g/company-logo_200_200/company-logo_200_200/0/1630527219302?e=1736380800&v=beta&t=4DuGKbRoOGZsksvvSIsUwllcciPGDAhRrboC-Ep9OHg" />
       </div>
       <div>
         <ul className="flex">
