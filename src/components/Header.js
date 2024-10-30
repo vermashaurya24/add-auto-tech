832bef6b8497154421be24b4884cb5b5d63679e2
vermashaurya24 <vermashaurya24@gmail.com>
2024-10-31 01:24:37 +0545

    chore: Design changes.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="flex justify-between items-center border-solid border-2 border-sky-500 rounded-xl bg-gradient-to-l from-blue-300">
+    <div className="flex justify-between text-lg items-center border-solid border-2 border-sky-500 rounded-xl bg-gradient-to-l from-blue-300">
       <div>
         <img className="w-24 rounded-2xl" src="https://media.licdn.com/dms/image/v2/C4D0BAQFeP0T2C9Io9g/company-logo_200_200/company-logo_200_200/0/1630527219302?e=1736380800&v=beta&t=4DuGKbRoOGZsksvvSIsUwllcciPGDAhRrboC-Ep9OHg" />
       </div>
