449f2a590a41d7c1515d8a74b9d41d04f98ba248
vermashaurya24 <vermashaurya24@gmail.com>
2024-10-31 13:15:06 +0545

    refactor: Refactored the Nav items into their own component, which all logic to handle responsiveness.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,12 @@
+import Nav from "./Nav";
+
 const Header = () => {
   return (
-    <div className="flex justify-between text-lg items-center border-solid border-2 border-sky-500 rounded-xl bg-gradient-to-l from-blue-300">
+    <div className="flex justify-between text-lg items-center z-[20] flex-wrap border-solid border-2 border-sky-500 rounded-xl bg-gradient-to-l from-blue-300">
       <div>
         <img className="w-24 rounded-2xl" src="https://media.licdn.com/dms/image/v2/C4D0BAQFeP0T2C9Io9g/company-logo_200_200/company-logo_200_200/0/1630527219302?e=1736380800&v=beta&t=4DuGKbRoOGZsksvvSIsUwllcciPGDAhRrboC-Ep9OHg" />
       </div>
-      <div>
-        <ul className="flex">
-          <li className="m-2 p-2">Home</li>
-          <li className="m-2 p-2">Projects</li>
-          <li className="m-2 p-2">Contact Us</li>
-          <li className="m-2 p-2">Store</li>
-        </ul>
-      </div>
+      <Nav />
     </div>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,47 @@
-import { NavLink } from "react-router-dom"
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import { FaBarsStaggered } from "react-icons/fa6";
+import { FaTimes } from 'react-icons/fa';
+import { HiOutlineCpuChip } from "react-icons/hi2";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdContacts } from "react-icons/md";
+import { BsCart2 } from "react-icons/bs";
+
+const NavLinks = () => {
+    return (
+        <>
+            <NavLink className="flex items-center pr-2 lg:pr-0" to="/">Home {<IoHomeOutline className="m-1" size={22} />}</NavLink>
+            <NavLink className="flex items-center pr-2 lg:pr-0" to="/projects">Projects {<HiOutlineCpuChip className="m-1" size={25} />}</NavLink>
+            <NavLink className="flex items-center pr-2 lg:pr-0" to="/contact">Contact {<MdContacts className="m-1" size={21} />}</NavLink>
+            <NavLink className="flex items-center pr-2 lg:pr-0" to="/store">Store {<BsCart2 className="m-1" size={21} />}</NavLink>
+        </>
+    )
+}
 
 const Nav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
     return (
-        <div>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/projects">Projects</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-            <NavLink to="/store">Store</NavLink>
-        </div>
+        <>
+            <div className="w-1/2 lg:w-1/3 flex justify-end">
+                <div className="hidden w-full md:flex justify-around pr-5">
+                    <NavLinks />
+                </div>
+                <div className="md:hidden w-1/5">
+                    <button onClick={toggleMenu}>{isMenuOpen? <FaTimes size={30} /> : <FaBarsStaggered size={30} />}</button>
+                </div>
+            </div>
+            {isMenuOpen && (
+                <div className="md:hidden flex flex-col my-2 items-center basis-full">
+                    <NavLinks />
+                </div>
+            )}
+        </>
     );
 };
 
