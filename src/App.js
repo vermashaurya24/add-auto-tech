f50ce96a8dac82d19280cd25b0f825c36f163ec9
vermashaurya24 <vermashaurya24@gmail.com>
2024-10-31 18:37:35 +0545

    feat: Added a state on the App level that tracks the dark/light mode status.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 
 const App = () => {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    setMode(prev=>prev==="light" ? "dark" : "light")
+  }
+
   return (
     <div className="mt-3 mx-3">
-      <Header />
+      <Header mode={mode} toggleMode={toggleMode} />
       <h1>Body Here</h1>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,20 +7,24 @@ import { HiOutlineCpuChip } from "react-icons/hi2";
 import { IoHomeOutline } from "react-icons/io5";
 import { MdContacts } from "react-icons/md";
 import { BsCart2 } from "react-icons/bs";
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
 
-const NavLinks = () => {
+const NavLinks = ({lightMode, toggleMode}) => {
     return (
         <>
-            <NavLink className="flex items-center pr-2 lg:pr-0" to="/">Home {<IoHomeOutline className="m-1" size={22} />}</NavLink>
-            <NavLink className="flex items-center pr-2 lg:pr-0" to="/projects">Projects {<HiOutlineCpuChip className="m-1" size={25} />}</NavLink>
-            <NavLink className="flex items-center pr-2 lg:pr-0" to="/contact">Contact {<MdContacts className="m-1" size={21} />}</NavLink>
-            <NavLink className="flex items-center pr-2 lg:pr-0" to="/store">Store {<BsCart2 className="m-1" size={21} />}</NavLink>
+            <button onClick={toggleMode} className="flex items-center pr-4">{lightMode ? <MdLightMode size={28} /> : <MdDarkMode size={28} />}</button>
+            <NavLink className="flex items-center pr-4" to="/">Home {<IoHomeOutline className="m-1" size={22} />}</NavLink>
+            <NavLink className="flex items-center pr-4" to="/projects">Projects {<HiOutlineCpuChip className="m-1" size={25} />}</NavLink>
+            <NavLink className="flex items-center pr-4" to="/contact">Contact {<MdContacts className="m-1" size={21} />}</NavLink>
+            <NavLink className="flex items-center" to="/store">Store {<BsCart2 className="m-1" size={21} />}</NavLink>
         </>
-    )
-}
+    );
+};
 
-const Nav = () => {
+const Nav = ({mode, toggleMode}) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const lightMode = mode === "light";
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -28,9 +32,9 @@ const Nav = () => {
 
     return (
         <>
-            <div className="w-1/2 lg:w-1/3 flex justify-end">
-                <div className="hidden w-full md:flex justify-around pr-6">
-                    <NavLinks />
+            <div className="w-1/2 lg:w-1/3 flex justify-end pr-4">
+                <div className="hidden w-auto md:flex justify-around pr-6">
+                    <NavLinks lightMode={lightMode} toggleMode={toggleMode} />
                 </div>
                 <div className="md:hidden w-1/5">
                     <button onClick={toggleMenu}>{isMenuOpen? <FaTimes size={30} /> : <FaBarsStaggered size={30} />}</button>
