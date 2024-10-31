7266b28e160a63e4adc72d096c95c427a166a1f3
vermashaurya24 <vermashaurya24@gmail.com>
2024-10-31 18:48:40 +0545

    chore: Removed extra unnecessary width css classes.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -32,11 +32,11 @@ const Nav = ({mode, toggleMode}) => {
 
     return (
         <>
-            <div className="w-1/2 lg:w-1/3 flex justify-end pr-4">
+            <div className="lg:w-1/3 flex justify-end pr-4">
                 <div className="hidden w-auto md:flex justify-around pr-6">
                     <NavLinks lightMode={lightMode} toggleMode={toggleMode} />
                 </div>
-                <div className="md:hidden w-1/5">
+                <div className="md:hidden">
                     <button onClick={toggleMenu}>{isMenuOpen? <FaTimes size={30} /> : <FaBarsStaggered size={30} />}</button>
                 </div>
             </div>
