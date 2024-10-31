48c28e87bb186ed26c0f92f3f46eec861b034e25
vermashaurya24 <vermashaurya24@gmail.com>
2024-11-01 00:35:23 +0545

    feat: Light mode backgound is slate instead of white.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   }
 
   return (
-    <div className={mode === "dark" && `h-dvh bg-slate-800`}>
+    <div className={mode === "dark" ? `h-dvh bg-slate-800` : `h-dvh bg-slate-400`}>
       <Header mode={mode} toggleMode={toggleMode} />
       <Outlet />
     </div>
