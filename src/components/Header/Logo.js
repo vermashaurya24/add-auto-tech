744e30db5d1cb7d36d7d8b839b83edbbfd973630
vermashaurya24 <vermashaurya24@gmail.com>
2024-11-03 18:44:27 +0545

    feat: Modified Logo component to now accept a width parameter to render dynamically at different places with different widths.

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,8 +1,8 @@
-const Logo = () => {
+const Logo = ({width = 24}) => {
   return (
     <div className="flex items-center">
       <img
-        className="w-24 rounded-2xl mr-4"
+        className={`w-${width} rounded-2xl mr-4`}
         src="https://media.licdn.com/dms/image/v2/C4D0BAQFeP0T2C9Io9g/company-logo_200_200/company-logo_200_200/0/1630527219302?e=1736380800&v=beta&t=4DuGKbRoOGZsksvvSIsUwllcciPGDAhRrboC-Ep9OHg"
       />
       <div className="font-medium font-serif text-2xl">Addauto Technology</div>
