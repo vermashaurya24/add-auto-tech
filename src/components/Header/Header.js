7662bea14ebec73969fdb8ddd902ceb221f28b27
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-01-14 15:29:01 -0500

    Header progressing--color changing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,9 @@ import { ModeContext } from "../../context/ModeContext";
 const Header = () => {
   const {mode, toggleMode} = useContext(ModeContext);
   //const borderColor = mode === "light" ? `border-sky-500` : `border-purple-500`;
-  const gradientFrom = mode === "light" ? `from-blue-300 to-[#5366C2]` : `from-violet-300`;
+  const gradientFrom = mode === "light" ? `from-blue-300 to-[#5366C2]` : `from-violet-300 to-[#9C27B0]`;
   return (
-    <div className={`flex justify-between text-lg items-center py-2 bg-gradient-to-l ${gradientFrom}`}>
+    <div className={`fixed top-0 left-0 z-50 w-full flex justify-between text-lg items-center py-2 bg-gradient-to-l ${gradientFrom}`}>
       <Logo />
       <Nav mode={mode} toggleMode={toggleMode} />
     </div>
