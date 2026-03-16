0cb8f40ba4c92fb61c9d642c5c29ff2f6cc2b935
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-03-16 00:27:17 -0400

    meet the team

--- src/components/MeetTheTeam.js
+++ src/components/MeetTheTeam.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import teamData from "../data/teamData";
+import { ModeContext } from "../context/ModeContext";
 const MeetTheTeam = () => {
+  const { mode } = useContext(ModeContext);
+
   return (
     <div className="pt-56 px-8 md:px-16 pb-32">
-      <section className="grid grid-cols-1 min-[900px]:grid-cols-2 min-[1470px]:grid-cols-3 gap-6">
+      <section className="grid grid-cols-1 min-[900px]:grid-cols-2 min-[1470px]:grid-cols-3 gap-8">
         {teamData.map((member, index) => {
           return (
-            <div className="shadow-[0_10px_25px_rgba(0,0,0,0.25)] p-6 rounded-2xl ">
+            <div
+              className={`${mode === "dark" ? "shadow-[0px_0px_15px_0px_rgba(239,24,111,0.50)] " : "shadow-[0_10px_25px_rgba(0,0,0,0.25)] "} p-6 rounded-2xl 
+              hover:shadow-[0_10px_25px_rgba(0,0,0,0.50)] transition-shadow duration-300 ease-in`}
+            >
               <div
                 key={index}
                 className="flex gap-6 max-[500px]:flex-col max-[500px]:gap-0 mb-6"
